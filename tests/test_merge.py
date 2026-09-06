3060ccaff404ff1f12b4d251c7d93086c369e67f
anT0ny54 <109664822+anT0ny54@users.noreply.github.com>
2026-09-06 23:47:51 +0700

    Add files via upload

--- tests/test_merge.py
+++ tests/test_merge.py
@@ -8,12 +8,13 @@ spec = importlib.util.spec_from_file_location("merge", SCRIPT)
 merge = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(merge)
 
-
-class ABPTests(unittest.TestCase):
+class ABPStrictTests(unittest.TestCase):
     def test_network_exception_is_preserved(self):
         rule = "@@||Example.COM^$SCRIPT,domain=example.com|~ads.example.com"
-        out = merge.normalize_rule(rule)
-        self.assertEqual(out, "@@||Example.COM^$domain=example.com|~ads.example.com,script")
+        self.assertEqual(
+            merge.normalize_rule(rule),
+            "@@||Example.COM^$domain=example.com|~ads.example.com,script",
+        )
 
     def test_cosmetic_exception_is_preserved(self):
         self.assertEqual(
@@ -25,13 +26,19 @@ class ABPTests(unittest.TestCase):
         rule = "example.com#?#div:-abp-has(.ad)"
         self.assertEqual(merge.normalize_rule(rule), rule)
 
-    def test_snippet_filter_is_preserved(self):
-        rule = "example.com##+js(set-constant, foo, true)"
-        self.assertEqual(merge.normalize_rule(rule), rule)
+    def test_ubo_extended_exception_is_rejected(self):
+        self.assertIsNone(merge.normalize_rule("example.com#?@#.ad"))
+
+    def test_ubo_snippet_is_rejected(self):
+        self.assertIsNone(merge.normalize_rule("example.com##+js(set-constant, foo, true)"))
 
     def test_regex_with_dollar_is_preserved(self):
-        rule = r"/foo\\$bar/$script"
-        self.assertEqual(merge.normalize_rule(rule), r"/foo\\$bar/$script")
+        rule = r"/foo\$bar/"
+        self.assertEqual(merge.normalize_rule(rule), rule)
+        anchored = r"/foo$/"
+        self.assertEqual(merge.normalize_rule(anchored), anchored)
+        with_option = r"/foo$/$match-case"
+        self.assertEqual(merge.normalize_rule(with_option), with_option)
 
     def test_options_are_case_normalized_and_sorted(self):
         self.assertEqual(
@@ -39,12 +46,31 @@ class ABPTests(unittest.TestCase):
             "||example.com^$image,third-party",
         )
 
-    def test_csp_option_with_spaces_is_preserved(self):
+    def test_csp_option_is_preserved(self):
         rule = "||example.com^$csp=script-src: 'none'"
         self.assertEqual(merge.normalize_rule(rule), rule)
 
     def test_duplicate_options_rejected(self):
         self.assertIsNone(merge.normalize_rule("||example.com^$script,script"))
+
+    def test_unknown_ubo_option_rejected(self):
+        self.assertIsNone(merge.normalize_rule("||example.com^$removeparam=utm_source"))
+
+    def test_document_only_exception(self):
+        self.assertIsNone(merge.normalize_rule("||example.com^$document"))
+        self.assertEqual(
+            merge.normalize_rule("@@||example.com^$document"),
+            "@@||example.com^$document",
+        )
+
+    def test_rewrite_requires_domain(self):
+        self.assertIsNone(merge.normalize_rule("||example.com^$rewrite=abp-resource:blank-js"))
+        self.assertEqual(
+            merge.normalize_rule(
+                "||example.com^$rewrite=abp-resource:blank-js,domain=example.com"
+            ),
+            "||example.com^$domain=example.com,rewrite=abp-resource:blank-js",
+        )
 
     def test_comments_and_hosts_are_rejected(self):
         self.assertIsNone(merge.normalize_rule("! comment"))
@@ -56,7 +82,5 @@ class ABPTests(unittest.TestCase):
             "https://example.com/child.txt",
         )
 
-
 if __name__ == "__main__":
     unittest.main(verbosity=2)
-               
